27370ce096ef173fe578601bcf54c838c2d3c1a6
neropatti <28650266+neropatti@users.noreply.github.com>
2022-02-03 18:30:06 +0200

    Add 3D vector visualization

--- addons/easy_velocity_vector_display/plugin.gd
+++ addons/easy_velocity_vector_display/plugin.gd
@@ -3,7 +3,9 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_custom_type("VectorDisplay2D", "Node2D", preload("vectordisplay2D.gd"), preload("icon.svg"))
+	add_custom_type("VectorDisplay3D", "Spatial", preload("vectordisplay3D.gd"), preload("icon.svg"))
 
 
 func _exit_tree():
 	remove_custom_type("VectorDisplay2D")
+	remove_custom_type("VectorDisplay3D")
